57bd91749ecc980d51606bcd03fe354ae659757a
ivarun66-stack <ivarun66@gmail.com>
2026-02-28 19:03:26 +0600

    Integrate Flask server with Telegram bot

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 import telebot
+import threading
+from flask import Flask
+import os
 
 TOKEN = "8788093402:AAHDbaKmBvHsT1RNcElMNVxbr1MjE8OmirE"
 
@@ -8,4 +11,16 @@ bot = telebot.TeleBot(TOKEN)
 def start(message):
     bot.reply_to(message, "Бот работает 🚀")
 
-bot.polling()
+# --- Flask сервер для Render ---
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run_bot():
+    bot.polling(none_stop=True)
+
+if __name__ == "__main__":
+    threading.Thread(target=run_bot).start()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
