635d48d075ed681e50b7d9bbda10953ce29f58d7
ivarun66-stack <ivarun66@gmail.com>
2026-02-28 18:11:32 +0600

    Add basic Telegram bot functionality

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
+import telebot
 
+TOKEN = "8788093402:AAHDbaKmBvHsT1RNcElMNVxbr1MjE8OmirE"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Бот работает 🚀")
+
+bot.polling()
